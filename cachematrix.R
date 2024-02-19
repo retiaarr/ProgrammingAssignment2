e4f3bf191ef5eadab986a5df518143686d867e9b
retiaarr <160466003+retiaarr@users.noreply.github.com>
2024-02-19 15:22:24 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+This function, makeCacheMatrix, is designed to create a closure that stores a matrix
+and its inverse.
 
-## Write a short comment describing this function
+This function creates a closure that acts as a cache for a matrix and its inverse, 
+allowing users to set and retrieve the matrix, as well as set and retrieve its
+inverse. The purpose of caching the inverse is to avoid recalculating it each time
+it is needed, which can be computationally expensive for large matrices.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        P <- NULL
+        set <- function(y){
+                x <<- y
+                p <<- NULL
+        }
+        get <- function()x
+        setInverse <- function(inverse) p <<- inverse
+        getInverse <- function() p
+        list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
+This function, cacheSolve, is designed to compute the inverse of a matrix 
+efficiently by utilizing caching. 
 
-## Write a short comment describing this function
+This function efficiently computes the inverse of a matrix by first checking if the
+inverse has already been computed and cached. If the inverse is found in the cache, 
+it is immediately returned. Otherwise, the inverse is computed, cached and returned. 
+This caching mechanism helps avoid redundant computations, especially for large 
+matrices, resulting in faster computation times when the same matrix is inverted
+multiple times.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+        p <- x$getInverse()
+        if(!is.null(p)){
+                message("getting cached data")
+                return(p)
+        }
+        mat <- x$get()
+        p <- x$get()
+        p$setInverse(j)
+        p
 }
